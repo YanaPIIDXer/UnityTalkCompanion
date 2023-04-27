88ce543fc63054fafa7a4ddf721d5811c92024c9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-28 08:39:16 +0900

    AIクラスをとりあえず定義するだけ定義

---
+++ TalkCompanion/Assets/Scripts/ChatGPT/AICore.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TalkCompanion.ChatGPT
+{
+    /// <summary>
+    /// AIクラス
+    /// </summary>
+    public class AICore : MonoBehaviour
+    {
+    }
+}
