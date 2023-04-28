6839956c1816b438e048bfac11ef57ef70bf1794
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-28 10:57:14 +0900

    入力を受けてChatGPTのAPIを叩く処理を実装

--- TalkCompanion/Assets/Scripts/Character/CharacterAI.cs
+++ TalkCompanion/Assets/Scripts/Character/CharacterAI.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TalkCompanion.ChatGPT;
 using TalkCompanion.Env;
+using TalkCompanion.UI;
+using Zenject;
+using UniRx;
 
 namespace TalkCompanion.Character
 {
@@ -15,5 +18,20 @@ namespace TalkCompanion.Character
         /// ChatGPTのコア
         /// </summary>
         private ChatGPTCore gptCore = new ChatGPTCore(Environment.Instance.OpenAIToken, true);
+
+        /// <summary>
+        /// InputPresenterのInject
+        /// </summary>
+        /// <param name="inputPresenter">InputPresenter</param>
+        [Inject]
+        public void OnInjectInputPresenter(IInputPresenter inputPresenter)
+        {
+            inputPresenter.onUserSubmit
+                          .Subscribe(async (message) =>
+                          {
+                              string response = await gptCore.Say(message);
+                              Debug.Log(response);
+                          }).AddTo(gameObject);
+        }
     }
 }
